c397c6b47a2cdc7df586d4ff1224f0606a7a4c97
jjc12 <jjc12@my.fsu.edu>
2016-06-29 20:35:29 -0400

    Write a for-loop for iterative processes.
    Write a for-loop in order to display an iterative process.

--- ArrayDemo/MyArray/MyArray.cpp
+++ ArrayDemo/MyArray/MyArray.cpp
@@ -15,8 +15,13 @@ int main()
 	m_aPrime[3] = 7;
 	m_aPrime[4] = 11;
 	
-	cout << "The lowest prime number is: " << m_aPrime[0] << "\n";
-	cout << "The Sum of the first 5 primes is: " << m_aPrime[0] + m_aPrime[1] + m_aPrime[2] + m_aPrime[3] + m_aPrime[4] << "\n";
+	cout << "The lowest prime number is: " << m_aPrime[0] << std::endl;
+	int total = 0;
+	for(int i = 0; i < 5; ++i)
+	{
+		total += m_aPrime[i];
+	}
+	cout << "The Sum of the first 5 primes is: " << total << std::endl;
 
 	return 0;
 }
